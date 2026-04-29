3b83811f952ba391e126751968c9eed207760f8a
Ahmad <ahmad@gmail.com>
2026-04-29 06:06:51 +0300

    Initial deploy

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -1,3 +1,6 @@
 const sidebar=document.getElementById("sidebar");
 const toggleBtn=document.getElementById("toggleBtn");
-toggleBtn.onclick=()=>{sidebar.classList.toggle("expanded");};
+
+toggleBtn.onclick=()=>{
+sidebar.classList.toggle("expanded");
+};
