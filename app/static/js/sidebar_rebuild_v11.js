bf2b579f65c8ec6a6fb1d9e2ac82798cf55df454
Ahmad <ahmad@gmail.com>
2026-04-29 13:58:33 +0300

    Initial deploy

--- app/static/js/sidebar_rebuild_v11.js
+++ app/static/js/sidebar_rebuild_v11.js
@@ -1,6 +1,6 @@
 
 (function() {
-  var buildId = "v21-purge-sidebar-css-20260429";
+  var buildId = "v22-support-command-layout-20260429";
   window.SOLARDEYE_SIDEBAR_BUILD = buildId;
 
   function shells() {
